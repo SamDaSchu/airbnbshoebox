4b0498072d4a91d224d8501ac7cc69470ff8d2b7
Samuel Schuster <samuel.david.schuster@gmail.com>
2018-03-15 17:47:44 +0000

    initial commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,10 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def Impressum
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get pages_about_url
+    assert_response :success
+  end
+
+  test "should get Impressum" do
+    get pages_Impressum_url
+    assert_response :success
+  end
+
+end
